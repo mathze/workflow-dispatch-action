a0003e41da57209b75af7099713152e36547a04e
Mathze <270275+mathze@users.noreply.github.com>
2021-11-27 20:41:54 +0100

    restructured request to json

--- src/main/kotlin/data/GhGraphClient.kt
+++ src/main/kotlin/data/GhGraphClient.kt
@@ -1,5 +1,6 @@
 package data
 
+import com.rnett.action.core.logger.debug
 import com.rnett.action.core.logger.info
 import com.rnett.action.httpclient.MutableHeaders
 import io.ktor.client.request.HttpRequestBuilder
@@ -14,7 +15,7 @@ import utils.actions.ActionEnvironment
 class GhGraphClient(token: String) : WsClient(token) {
 
   suspend fun sendQuery(query: String, variables: JsonObject? = null): JsonElement {
-    info("Sending request >>$query<< to $graphApiUrl")
+    debug("Sending request >>$query<< to $graphApiUrl")
     val req = buildJsonObject {
       put("query", JsonPrimitive(query))
       variables?.let {
@@ -23,8 +24,9 @@ class GhGraphClient(token: String) : WsClient(token) {
     }
 
     val response = client.post(graphApiUrl, req.toString())
-    info("Response ${response.readBody()}")
-    return response.toJson()
+    val result = response.toJson()
+    info("Response $result")
+    return result
   }
 
   private val graphApiUrl by lazy {
