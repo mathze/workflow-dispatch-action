999e26b3639a8418cb3f841a8c050535241be3a6
Mathze <270275+mathze@users.noreply.github.com>
2022-01-11 22:20:37 +0100

    clean-ups on gradle scripts;

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,14 @@
-import com.rnett.action.generateAutoBuildWorkflow
 import com.rnett.action.githubAction
 
 plugins {
   kotlin("js")
-//  kotlin("plugin.serialization")
   id("com.github.rnett.ktjs-github-action") version "1.4.3"
 }
 
 repositories {
   mavenCentral()
-  // required by kotlinx-nodejs (see https://github.com/Kotlin/kotlinx-nodejs/issues/16)
+  // required by task `packageJson` -> config `npm` -> dependency `kotlinx-nodejs`
+  // (see https://github.com/Kotlin/kotlinx-nodejs/issues/16)
   jcenter()
 }
 
@@ -19,14 +18,11 @@ kotlin {
   }
 }
 
-generateAutoBuildWorkflow()
-
 dependencies {
   listOf("kotlin-js-action", "serialization").forEach {
     implementation(group = "com.github.rnett.ktjs-github-action", name = it, version = "1.4.3")
   }
 
-  implementation(group="app.softwork", name="kotlinx-uuid-core-js", version="0.0.9")
-  implementation(kotlin("stdlib-js"))
-  implementation("io.ktor:ktor-client-js:1.6.4")
+  implementation(group = "app.softwork", name = "kotlinx-uuid-core-js", version = "0.0.12")
+  implementation(group = "io.ktor", name = "ktor-client-js", version = "1.6.6")
 }
