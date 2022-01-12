252028de0aae2dcff3e68bce49270ba329c0f791
Mathze <270275+mathze@users.noreply.github.com>
2022-01-12 19:51:27 +0100

    Revert "added missing dependency"
    This reverts commit 6c53d2eb06375104ff3907b3a6432b556fe33968.

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,6 @@ dependencies {
   implementation(group="com.github.rnett.ktjs-github-action", name="kotlin-js-action", version="1.4.3")
   implementation(kotlin("stdlib-js"))
 
-  implementation(npm("abort-controller", "3.0.0"))
   implementation("io.ktor:ktor-client-js:1.6.4")
   implementation("io.ktor:ktor-client-serialization:1.6.4")
   implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.1")
