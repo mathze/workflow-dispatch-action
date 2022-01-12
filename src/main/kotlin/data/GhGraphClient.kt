b2b08a1fad5f5f487441e269642302387b994c41
Mathze <270275+mathze@users.noreply.github.com>
2022-01-12 19:51:27 +0100

    Revert "fixed sending requests"
    This reverts commit 61c60f13f558b0d8a77908466fba708a8b610708.

--- src/main/kotlin/data/GhGraphClient.kt
+++ src/main/kotlin/data/GhGraphClient.kt
@@ -1,6 +1,5 @@
 package data
 
-import com.rnett.action.core.logger.info
 import com.rnett.action.httpclient.MutableHeaders
 import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.http.HttpHeaders
@@ -11,7 +10,6 @@ import utils.actions.ActionEnvironment
 class GhGraphClient(token: String) : WsClient(token) {
 
   suspend fun sendQuery(query: String): JsonElement {
-    info("Sending request >>$query<< to $graphApiUrl")
     return client.post(graphApiUrl, query).toJson()
   }
 
