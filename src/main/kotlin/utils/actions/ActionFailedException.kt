59940029450677d7df2d62e8f61202b27778a03c
Mathze <270275+mathze@users.noreply.github.com>
2022-01-12 19:51:27 +0100

    Revert "init commit"
    This reverts commit 5fa85613735a1e83af2dc3b9628e05929118f63b.

--- src/main/kotlin/utils/actions/ActionFailedException.kt
+++
@@ -1,5 +0,0 @@
-package utils.actions
-
-class ActionFailedException(override val message: String?, override val cause: Throwable?): Throwable(message, cause) {
-  constructor(message: String?): this(message, null)
-}
