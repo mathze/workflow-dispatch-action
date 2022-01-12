50fa1fba57d56bf50caad4f847dadd22355dc263
Mathze <270275+mathze@users.noreply.github.com>
2022-01-12 19:51:27 +0100

    Revert "next try token retrieval"
    This reverts commit 9e99fd63008aad56e79ff71d84b542572d9110ad.

--- src/main/kotlin/model/Inputs.kt
+++ src/main/kotlin/model/Inputs.kt
@@ -35,9 +35,7 @@ data class Inputs(
         inputs.getOptional("ref"),
         inputs.getRequired("workflow-name"),
         Json.parseToJsonElement(inputs.getOrElse("payload") { "{}" }).jsonObject,
-        inputs.getOrElse("token") { ActionEnvironment.GITHUB_TOKEN }.also {
-          logger.info("Token: $this")
-        },
+        inputs.getRequired("token").apply { maskSecret() },
         inputs.getOptional("fail-on-error")?.toBooleanStrictOrNull() ?: false,
         inputs.getOptional("use-marker-step")?.toBooleanStrictOrNull() ?: false,
         inputs.getOptional("run-id"),
