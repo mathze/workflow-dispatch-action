d95cdd086cf8ad2e05fd13e2784a1dacea6b806d
Mathze <270275+mathze@users.noreply.github.com>
2022-01-12 19:51:27 +0100

    Revert "fixed graphQl query-request-builder"
    This reverts commit 2bfc565498a0e5f177e55edf031c572a5f7f214f.

--- src/main/kotlin/data/GhGraphClient.kt
+++ src/main/kotlin/data/GhGraphClient.kt
@@ -6,23 +6,13 @@ import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.http.HttpHeaders
 import io.ktor.http.takeFrom
 import kotlinx.serialization.json.JsonElement
-import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.JsonPrimitive
-import kotlinx.serialization.json.buildJsonObject
 import utils.actions.ActionEnvironment
 
 class GhGraphClient(token: String) : WsClient(token) {
 
-  suspend fun sendQuery(query: String, variables: JsonObject? = null): JsonElement {
+  suspend fun sendQuery(query: String): JsonElement {
     info("Sending request >>$query<< to $graphApiUrl")
-    val req = buildJsonObject {
-      put("query", JsonPrimitive(query))
-      variables?.let {
-        this.put("variables", variables)
-      }
-    }
-
-    val response = client.post(graphApiUrl, req.toString())
+    val response = client.post(graphApiUrl, query)
     info("Response ${response.readBody()}")
     return response.toJson()
   }
@@ -30,12 +20,10 @@ class GhGraphClient(token: String) : WsClient(token) {
   private val graphApiUrl by lazy {
     ActionEnvironment.GITHUB_GRAPHQL_URL
   }
-
   override fun applyGhDefaults(headers: MutableHeaders) {
     super.applyGhDefaults(headers)
     headers.add(HttpHeaders.Accept, "application/json")
   }
-
   private fun HttpRequestBuilder.ghDefaults() {
     url {
       takeFrom(graphApiUrl)
