5629c3a65fef54e4ce5f3b68a1235e41c17edbb9
Mathze <270275+mathze@users.noreply.github.com>
2024-03-11 20:51:01 +0100

    #114: Update to node 20

--- src/jsMain/kotlin/utils/actions/ActionFailedException.kt
+++ src/jsMain/kotlin/utils/actions/ActionFailedException.kt
@@ -3,6 +3,4 @@ package utils.actions
 /**
  * Exception to indicate a controlled action exit.
  */
-class ActionFailedException(message: String?, cause: Throwable?): Throwable(message, cause) {
-  constructor(message: String?): this(message, null)
-}
+class ActionFailedException(message: String?, cause: Throwable? = null): Throwable(message, cause)
